ea05e9fdefae4b30ba283057583349122caef51f
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-01 01:33:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,6 +7,7 @@ require_relative '../lib/concerns/paramable'
 class Artist
   extend Memorable::ClassMethods
   extend Memorable::InstanceMethods
+  extend Findable
   include Paramable
   attr_accessor :name
   attr_reader :songs
